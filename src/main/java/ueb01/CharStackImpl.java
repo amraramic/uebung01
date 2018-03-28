551e80738664ff726e0d2b1b43c3012a53ce02b4
Amra Ramic <Amra.Ramic@stud.fh-rosenheim.de>
2018-03-28 11:15:03 +0100

    Musst noch was machen!

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,30 @@
 package ueb01;
 
 class CharStackImpl implements CharStack {
-}
+    private Element first = null;
+
+    @Override
+    public void push(char c) {
+        Element neu = new Element(c, first);
+        first = neu;
+    }
+
+    @Override
+    public char pop() {
+        Element it = first;
+        it.back = first;
+        return first.value;
+    }
+
+    @Override
+    public int size() {
+        Element it = first;
+        int len = 0;
+            while (it != null) {
+                it = it.back;
+                len++;
+            }
+            return len;
+        }
+    }
+
---
+++ src/main/java/ueb01/Element.java
@@ -0,0 +1,10 @@
+package ueb01;
+
+public class Element {
+    char value;
+    Element back;
+    Element (char v, Element b){
+        value = v;
+        back = b;
+    }
+}
