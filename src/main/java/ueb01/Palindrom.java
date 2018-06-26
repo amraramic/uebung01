44afd4684dcfba51116fe4f6c86f95b2512bf1c7
Amra Ramic <Amra.Ramic@stud.fh-rosenheim.de>
2018-06-26 22:09:05 +0100

    done!

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -2,8 +2,27 @@ package ueb01;
 
 class Palindrom {
 	static boolean istPalindrom(String s) {
-		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+
+		if (s.length() == 1) {
+			return true;
+		}
+		CharStack erstehaelfte = new CharStackImpl();
+			char [] chararr = s.replaceAll(" ", "").toLowerCase().toCharArray();
+			int i = 0;
+
+			for (; i < chararr.length / 2; i++) {
+				erstehaelfte.push(chararr[i]);
+			}
+
+			if (chararr.length % 2  == 1){
+				i++;
+			}
+			for (; i < chararr.length; i++){
+				if (erstehaelfte.pop() != chararr[i]) {
+					return false;
+				}
+			}
+			return true;
 	}
 }
